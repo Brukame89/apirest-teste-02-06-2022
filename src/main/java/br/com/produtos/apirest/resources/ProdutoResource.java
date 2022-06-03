cee42a6e2f632fd3e080975b5a8ead4289ec7115
Brukame89 <bruno.oliveirasp89@gmail.com>
2022-06-03 17:19:54 -0300

    Pequenas alterações 03-06-2022

--- src/main/java/br/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/br/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,8 +3,11 @@ package br.com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +22,25 @@ public class ProdutoResource {
     ProdutoRepository produtoRepository;
 
     @GetMapping("/produtos") //Método "Get", lista todos os produtos salvos no banco de dados
-    public List<Produto> listaProdutos(){ // O método "listaProdutos" vai retornar uma lista de produtos, 
+    public List<Produto> listaProdutos(){ // O método "listaProdutos" vai retornar uma lista de produtos quando for feita a requisição 
         return produtoRepository.findAll();
     }
 
-    @GetMapping("/produtos/{id}") 
-    public Produto listaProdutoUnico(@PathVariable(value = "id") long id){ //usado para selecionar um produto específico pelo "id" deste produto
+    @GetMapping("/produtos/{id}") //usado para selecionar um produto específico pelo "id" deste produto
+    public Produto listaProdutoUnico(@PathVariable(value = "id") long id){ 
         return produtoRepository.findById(id);
     }
     
+    @PostMapping("/produto") //usado para salvar um novo produto
+    public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+
+    }
+
+    @DeleteMapping("/produto") //usado para salvar um novo produto
+    public void deletaProduto(@RequestBody Produto produto){
+        produtoRepository.delete(produto);
+
+    }
+
 }
